9a71755e498f3cd6e812d78baff40cf4044e6b12
LironKruchininGGo <lironkruch@gmail.com>
2023-07-27 00:51:41 +0300

    connectivity for BE getting user data

--- src/views/AboutPage.tsx
+++ src/views/AboutPage.tsx
@@ -1,11 +1,33 @@
 import { motion } from 'framer-motion'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { apiPostRequest } from '../services/api'
+import { UserData } from '../interface/User'
 
 type Props = {}
 
 const AboutPage = (props: Props) => {
+    const [users, setUsers] = useState<[UserData]>()
+
+    useEffect(() => {
+        getUsers()
+        console.log(users);
+
+    }, [])
+
+    const getUsers = async () => {
+        const users = await apiPostRequest(`${process.env.REACT_APP_LOCAL_API_URL}/users`, [])
+        setUsers(users)
+    }
+
+
     return (
-        <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} style={{ position: 'absolute', top: 80, left: 0, right: 0, bottom: 0 }}>AboutPage</motion.div>
+        <motion.div initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            style={{ position: 'absolute', top: 80, left: 0, right: 0, bottom: 0 }}
+        >
+            {users?.map(user => { return <div>{user.email}</div> })}
+        </motion.div>
     )
 }
 
