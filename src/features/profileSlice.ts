0750770b2d646ead41535a311edbc7d842aac676
LironKruchininGGo <lironkruch@gmail.com>
2023-08-11 15:07:02 +0300

    addewd cases for when user is logged in

--- src/features/profileSlice.ts
+++ src/features/profileSlice.ts
@@ -1,39 +1,57 @@
-// slice.js
 import { createAsyncThunk, createSlice, isRejectedWithValue } from '@reduxjs/toolkit';
 import { getLocalStorage } from '../utils/localStorage';
 import { apiPostRequest } from '../services/api';
 
-export const fetchUserData = createAsyncThunk('userData/post', async (email: string) => {
+export const fetchUserData = createAsyncThunk('userData/post', async (email: string | undefined) => {
     try {
-        console.log('fetching email', email);
-
         const accessToken = getLocalStorage('accessToken')
-        console.log('accessToken', accessToken);
-
         const data = await apiPostRequest(`${process.env.REACT_APP_LOCAL_API_URL}/profile`,
             { email: email },
             { Authorization: `Bearer ${JSON.parse(accessToken)}` })
-        console.log('data', data);
+        console.log('data', data)
 
         return data
     }
     catch (err) {
-        console.log('failed getting user data');
+        console.log('failed getting user data')
 
         throw err
     }
 })
 
-const initialState = {
+interface UserProfileState {
+    data: UserData | null;
+    isAuthenticated: boolean;
+    isLoading: boolean;
+    error: string | null;
+}
+
+interface UserData {
+    email: string;
+    // ... other fields of the user data
+}
+
+
+const initialState: UserProfileState = {
     data: null,
+    isAuthenticated: false,
     isLoading: false,
-    error: null
-};
+    error: null as string | null
+}
 
 const userSlice = createSlice({
     name: 'userData',
     initialState,
-    reducers: {},
+    reducers: {
+        loginUser: (state, action) => {
+            state.isAuthenticated = true
+            state.data = action.payload
+        },
+        logoutUser: (state) => {
+            state.isAuthenticated = false
+            state.data = null
+        }
+    },
     extraReducers(builder) {
         builder
             .addCase(fetchUserData.pending, (state) => {
@@ -42,10 +60,17 @@ const userSlice = createSlice({
             })
             .addCase(fetchUserData.fulfilled, (state, action) => {
                 state.isLoading = false
-                // state.data = action.payload
+                state.data = action.payload
+                state.isAuthenticated = true
+            })
+            .addCase(fetchUserData.rejected, (state, action) => {
+                state.isLoading = false
+                state.isAuthenticated = false
+                state.error = action.error.message || 'Failed to fetch user data'
             })
     },
-});
+})
 
-export const { actions } = userSlice;
+export const { actions } = userSlice
+export const { loginUser, logoutUser } = userSlice.actions
 export default userSlice.reducer
