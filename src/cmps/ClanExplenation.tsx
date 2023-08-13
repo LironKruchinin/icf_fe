8f90f4c2bcb1f581f42ba10951e9228ff6477326
LironKruchininGGo <lironkruch@gmail.com>
2023-08-13 22:42:17 +0300

    dynamic clanexplenation page

--- src/cmps/ClanExplenation.tsx
+++ src/cmps/ClanExplenation.tsx
@@ -1,14 +1,15 @@
 import React from 'react'
 
-const ClanExplenation = () => {
+interface ExplenationProps {
+    paragraphTitle?: string
+    paragraphText: string
+}
+
+const ClanExplenation: React.FC<ExplenationProps> = ({ paragraphTitle, paragraphText }) => {
     return (
         <div className='explenation'>
-            <h2>Embrace Realism, Unleash Fun: Join Our IDF-Inspired Clan!</h2>
-            <p>
-                We are a realism-focused clan that values experiences and fun.
-                With real IDF operators, we simulate diverse capabilities and maintain high professionalism.
-                Join us for an immersive and engaging combat experience.
-            </p>
+            <h2>{paragraphTitle}</h2>
+            <p>{paragraphText}</p>
         </div>
     )
 }
