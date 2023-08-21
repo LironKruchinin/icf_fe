032bfe9a6e57b0272c937f3972f678f74a64feb6
LironKruchininGGo <lironkruch@gmail.com>
2023-08-21 21:13:27 +0300

    fixed logout + signup bug

--- src/cmps/AppHeader.tsx
+++ src/cmps/AppHeader.tsx
@@ -1,27 +1,33 @@
 import { useEffect, useState } from 'react';
 import { BsPersonCircle } from 'react-icons/bs';
 import { HiMoon, HiSun } from "react-icons/hi2";
-import { useSelector } from 'react-redux';
-import { NavLink } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { NavLink, useNavigate } from 'react-router-dom';
 import icfImage from '../assets/images/icf_logo.png';
 import { Links } from '../interface/Header';
-import { RootState, persistor } from '../store/store';
-import { getLocalStorage, setLocalStorage } from '../utils/localStorage';
+import { AppDispatch, RootState, persistor } from '../store/store';
+import { getLocalStorage, removeLocalStorageKey, setLocalStorage } from '../utils/localStorage';
 import ToggleSwitch from './ToggleSwitch';
 import { getCookie, removeCookie } from '../utils/Cookie';
 import { logoutUser } from '../features/profileSlice';
+import UserPic from './UserPic';
+import LoadingSpinner from './LoadingSpinner';
 
 const AppHeader = () => {
+    const dispatch: AppDispatch = useDispatch();
     const [isDarkmode, setIsDarkMode] = useState<boolean | null>(getLocalStorage('isDarkMode'))
     const screenState = useSelector((state: RootState) => state.userProfile)
+    const [isLoggingOut, setIsLoggingOut] = useState(false)
     const hasAuthCookie = getCookie('accessToken')
+    const timeout = 1000
+    const navigate = useNavigate()
     const links: Links[] = [
         ...(screenState.isAuthenticated ? [
-            { pagePath: `/profile/${screenState.data?._id}`, linkName: <BsPersonCircle /> },
+            { pagePath: `/profile/${screenState.data?._id}`, linkName: <UserPic username={screenState.data?.first_name} userColor={screenState.data?.user_color} /> },
             { pagePath: '/donate', linkName: 'Donate' },
             { pagePath: '/about', linkName: 'About' },
             { pagePath: '/contact', linkName: 'Contact' },
-            { pagePath: '/logout', linkName: 'Sign out' },
+            // { pagePath: '/logout', linkName: 'Sign out' },
         ] : [
             { pagePath: '/login', linkName: 'Login' },
             { pagePath: '/register', linkName: 'Register' },
@@ -29,12 +35,8 @@ const AppHeader = () => {
             { pagePath: '/contact', linkName: 'Contact' },
         ])
     ]
-    console.log('roles', screenState);
 
-    const hasAdminRole = screenState.data?.roles.includes('owner') || screenState.data?.roles.includes('admin') ? true : false
-    console.log('hasAdminRole', hasAdminRole);
-
-    if (hasAdminRole) links.push({ pagePath: '/admin-panel', linkName: 'Panel' })
+    if (screenState.data && (screenState.data.roles?.includes('owner') || screenState.data.roles?.includes('admin'))) links.push({ pagePath: '/admin-panel', linkName: 'Panel' })
 
     useEffect(() => {
         document.body.className = isDarkmode ? 'dark-mode' : 'light-mode'
@@ -45,8 +47,21 @@ const AppHeader = () => {
         setLocalStorage('isDarkMode', isOn)
     }
 
+    const signOut = () => {
+        setIsLoggingOut(true)
+        removeCookie('accessToken')
+        removeLocalStorageKey('accessToken')
+        dispatch(logoutUser())
+        persistor.purge()
+        setTimeout(() => {
+            navigate('/')
+            setIsLoggingOut(false)
+        }, timeout);
+    }
+
     return (
         <nav>
+            {isLoggingOut && <LoadingSpinner />}
             <ul>
                 <div className='left-side'>
                     <li>
@@ -68,6 +83,11 @@ const AppHeader = () => {
                             <li>{link.linkName}</li>
                         </NavLink>
                     ))}
+                    {screenState.isAuthenticated && (
+                        <a>
+                            <li onClick={signOut}>Sign out</li>
+                        </a>
+                    )}
                 </div>
             </ul>
         </nav>
