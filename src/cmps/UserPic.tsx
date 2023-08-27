067eafc1f1243610ee07a0e29a3523f4bc963246
LironKruchininGGo <lironkruch@gmail.com>
2023-08-27 18:54:25 +0300

    added user picture

--- src/cmps/UserPic.tsx
+++ src/cmps/UserPic.tsx
@@ -7,6 +7,19 @@ type UserPicProps = {
 
 const UserPic: React.FC<UserPicProps> = ({ username, userColor }) => {
 
+    const getFontColor = (backgroundColor: string): string => {
+        const color = backgroundColor.substring(1)  // strip #
+        const rgb = parseInt(color, 16)
+        const r = (rgb >> 16) & 0xff
+        const g = (rgb >> 8) & 0xff
+        const b = (rgb >> 0) & 0xff
+
+
+        const luminance = 0.2126 * r + 0.7152 * g + 0.0722 * b // relative luminance formula
+        console.log(luminance < 128 ? 'white' : 'black');
+        return luminance < 128 ? 'white' : 'black'  // 128 is mid-point in RGB scale
+    }
+
     const profilePic = () => {
 
         if (username && (username.length > 2)) {
@@ -16,7 +29,7 @@ const UserPic: React.FC<UserPicProps> = ({ username, userColor }) => {
     }
 
     return (
-        <div className='user-pic' style={{ backgroundColor: userColor }} >
+        <div className='user-pic' style={{ backgroundColor: userColor, color: getFontColor(userColor || '#FFFFFF') }} >
             {profilePic()}
         </div>
     )
