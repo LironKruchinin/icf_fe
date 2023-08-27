f365fc7b55bc90c26ef458e917edde90f6bf49d1
LironKruchininGGo <lironkruch@gmail.com>
2023-08-27 18:54:18 +0300

    added hebrew translation, and logout when cookie expires

--- src/cmps/AppHeader.tsx
+++ src/cmps/AppHeader.tsx
@@ -1,48 +1,76 @@
-import { useEffect, useState } from 'react';
-import { BsPersonCircle } from 'react-icons/bs';
+import { useCallback, useEffect, useState } from 'react';
+import { useTranslation } from 'react-i18next';
 import { HiMoon, HiSun } from "react-icons/hi2";
 import { useDispatch, useSelector } from 'react-redux';
 import { NavLink, useNavigate } from 'react-router-dom';
 import icfImage from '../assets/images/icf_logo.png';
+import { logoutUser } from '../features/profileSlice';
 import { Links } from '../interface/Header';
 import { AppDispatch, RootState, persistor } from '../store/store';
-import { getLocalStorage, removeLocalStorageKey, setLocalStorage } from '../utils/localStorage';
-import ToggleSwitch from './ToggleSwitch';
 import { getCookie, removeCookie } from '../utils/Cookie';
-import { logoutUser } from '../features/profileSlice';
-import UserPic from './UserPic';
+import { getLocalStorage, removeLocalStorageKey, setLocalStorage } from '../utils/localStorage';
 import LoadingSpinner from './LoadingSpinner';
+import ToggleSwitch from './ToggleSwitch';
+import UserPic from './UserPic';
 
 const AppHeader = () => {
-    const dispatch: AppDispatch = useDispatch();
+    const dispatch: AppDispatch = useDispatch()
+    const { t, i18n } = useTranslation()
     const [isDarkmode, setIsDarkMode] = useState<boolean | null>(getLocalStorage('isDarkMode'))
     const screenState = useSelector((state: RootState) => state.profile)
     const [isLoggingOut, setIsLoggingOut] = useState(false)
-    const hasAuthCookie = getCookie('accessToken')
+    const authCookie = getCookie('accessToken')
     const timeout = 1000
     const navigate = useNavigate()
     const links: Links[] = [
         ...(screenState.isAuthenticated ? [
             { pagePath: `/profile/${screenState.data?._id}`, linkName: <UserPic username={screenState.data?.first_name} userColor={screenState.data?.user_color} /> },
-            { pagePath: '/donate', linkName: 'Donate' },
-            { pagePath: '/about', linkName: 'About' },
-            { pagePath: '/contact', linkName: 'Contact' },
-            // { pagePath: '/logout', linkName: 'Sign out' },
+            { pagePath: '/donate', linkName: 'donate' },
+            { pagePath: '/about', linkName: 'about' },
+            { pagePath: '/contact', linkName: 'contact' },
         ] : [
-            { pagePath: '/login', linkName: 'Login' },
-            { pagePath: '/register', linkName: 'Register' },
-            { pagePath: '/about', linkName: 'About' },
-            { pagePath: '/contact', linkName: 'Contact' },
+            { pagePath: '/login', linkName: 'login' },
+            { pagePath: '/register', linkName: 'register' },
+            { pagePath: '/about', linkName: 'about' },
+            { pagePath: '/contact', linkName: 'contact' },
         ])
     ]
-
-    if (screenState.data && (screenState.data.roles?.includes('owner') || screenState.data.roles?.includes('admin'))) links.push({ pagePath: '/admin-panel', linkName: 'Panel' })
+    const languages = [
+        { code: 'en', label: 'En' },
+        { code: 'he', label: 'He' }
+    ]
+    const checkCookieExpiration = useCallback(() => {
+        const currentAuthCookie = getCookie('accessToken');
+        if (!currentAuthCookie) {
+            console.log('removing cookie');
+            signOut();
+        }
+    }, []);
+    if (screenState.data && (screenState.data.roles?.includes('owner') || screenState.data.roles?.includes('admin'))) links.push({ pagePath: '/admin-panel', linkName: 'panel' })
 
     useEffect(() => {
+        checkCookieExpiration()
         document.body.className = isDarkmode ? 'dark-mode' : 'light-mode'
-        console.log(screenState);
 
-    }, [isDarkmode, screenState])
+        document.addEventListener("visibilitychange", () => {
+            console.log('authCookie', authCookie);
+            if (!document.hidden) {
+                checkCookieExpiration()
+            }
+        })
+        return () => {
+            document.removeEventListener("visibilitychange", checkCookieExpiration);
+        }
+    }, [checkCookieExpiration, isDarkmode, screenState]);
+
+    // useEffect(() => {
+    //     document.body.className = isDarkmode ? 'dark-mode' : 'light-mode'
+    // }, [isDarkmode, screenState])
+
+    const changeLanguage = (langCode: string) => {
+        i18n.changeLanguage(langCode)
+        document.documentElement.dir = langCode === 'he' ? 'rtl' : 'ltr'
+    }
 
     const toggleDarkMode = (isOn: boolean) => {
         setIsDarkMode(isOn)
@@ -58,13 +86,14 @@ const AppHeader = () => {
         setTimeout(() => {
             navigate('/')
             setIsLoggingOut(false)
-        }, timeout);
+        }, timeout)
     }
 
     return (
         <nav>
             {isLoggingOut && <LoadingSpinner />}
             <ul>
+
                 <div className='left-side'>
                     <li>
                         <NavLink to={'/'}>
@@ -73,6 +102,7 @@ const AppHeader = () => {
                         </NavLink>
                     </li>
                 </div>
+
                 <div className="links">
                     <ToggleSwitch
                         onToggle={toggleDarkMode}
@@ -80,14 +110,30 @@ const AppHeader = () => {
                         unCheckedIcon={<HiSun />}
                         isOn={isDarkmode}
                     />
-                    {links.map((link, index) => (
-                        <NavLink to={link.pagePath} key={index}>
-                            <li>{link.linkName}</li>
+
+                    <div className="language-switcher">
+                        {languages.map(lang => (
+                            <button key={lang.code} onClick={() => changeLanguage(lang.code)}>
+                                {lang.label}
+                            </button>
+                        ))}
+                    </div>
+
+                    {links.map((link, index) => {
+                        if (index === 0) {
+                            return <NavLink to={link.pagePath} key={index}>
+                                <li>{link.linkName}</li>
+                            </NavLink>
+                        }
+
+                        return <NavLink to={link.pagePath} key={index}>
+                            <li>{t(`header.${link.linkName}`)}</li>
                         </NavLink>
-                    ))}
+                    })}
+
                     {screenState.isAuthenticated && (
                         <a>
-                            <li onClick={signOut}>Sign out</li>
+                            <li onClick={signOut}>{t('header.signOut')}</li>
                         </a>
                     )}
                 </div>
