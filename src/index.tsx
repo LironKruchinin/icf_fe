cdc02f3c9a3b3fff0c8524f698e2ae8fe30ef3d3
LironKruchininGGo <lironkruch@gmail.com>
2023-08-27 18:53:52 +0300

    added hebrew translation

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,34 @@
 import ReactDOM from 'react-dom/client';
+import { initReactI18next } from 'react-i18next';
+import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
 import './assets/styles/main.scss';
 import AnimatedRoutes from './cmps/AnimatedRoutes';
-import AppHeader from './cmps/AppHeader';
-import { Provider } from 'react-redux';
-import { store, persistor } from './store/store';
 import AppFooter from './cmps/AppFooter';
-import { PersistGate } from 'redux-persist/integration/react';
+import AppHeader from './cmps/AppHeader';
+import en from './i18n/locales/en/translation.json';
+import he from './i18n/locales/he/translation.json';
+import { persistor, store } from './store/store';
+import i18n from 'i18next'
+
+i18n
+  .use(initReactI18next)
+  .init({
+    resources: {
+      en: {
+        translation: en
+      },
+      he: {
+        translation: he
+      }
+    },
+    lng: 'en',
+    fallbackLng: 'eng',
+    interpolation: {
+      escapeValue: false
+    }
+  })
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
